7ec5cd917de2eb4447a5adbdbec0b187353cbe2d
gusjak <70512255+gusjak@users.noreply.github.com>
2021-04-16 13:10:50 +0200

    React + Tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-gradient-to-l from-indigo-800 to-purple-600 font-sans">
+      <h1 className="font-bold tracking-wider text-white text-2xl">Brewdog React App 🍻</h1>
+    </div>
+  );
+}
+
+export default App;
