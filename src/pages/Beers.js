f7916e694e88af0f7289a69e1e976e5713798100
gusjak <70512255+gusjak@users.noreply.github.com>
2021-04-20 09:26:16 +0200

    Grid fix

--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -3,8 +3,8 @@ import ListOfBeers from '../components/ListOfBeers'
 
 const Beers = (props) => {
     return (
-        <div className="w-full flex flex-col justify-center items-center bg-gradient-to-l from-indigo-800 to-purple-600 font-sans">
-            <div className="w-full grid grid-cols-1 gap-10 mt-40 md:grid-cols-2 lg:grid-cols-3">
+        <div className="flex flex-col justify-center items-center bg-gradient-to-l from-indigo-800 to-purple-600 font-sans">
+            <div className="grid grid-cols-1 gap-10 mt-40 md:grid-cols-2 lg:grid-cols-3">
                 {props.allBeers.map((e) => (
                     <ListOfBeers key={e.id} beer={e} />
                 ))}
