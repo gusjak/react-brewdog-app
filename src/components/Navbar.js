a1da257b4a738202f18539c77629da84101c4f81
gusjak <70512255+gusjak@users.noreply.github.com>
2021-04-20 09:46:39 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,20 @@ function Navbar() {
     return (
         <>
             <nav className="absolute w-full flex flex-col text-center sm:flex-row py-4 px-10 sm:text-left sm:justify-between">
-                <div className="mb-6 sm:mb-0">
-                    <Link to="/" className="text-3xl text-white ml-4">
-                        Brewdog React App
-                    </Link>
+                <div className="flex flex-col mb-2 sm:mb-6">
+                    <h1 className="font-semibold text-3xl tracking-wide text-white ml-4">Brewdog React</h1>
+                    <a
+                        href="https://punkapi.com/documentation/v2"
+                        className="text-sm text-white ml-4 hover:text-pink-300"
+                    >
+                        https://punkapi.com/documentation/v2
+                    </a>
                 </div>
                 <div>
-                    <Link to="/" className="text-lg text-white hover:text-pink-300">
+                    <Link to="/" className="text-xl text-white mr-4 hover:text-pink-300">
                         Home
                     </Link>
-                    <Link to="/beers" className="text-lg text-white ml-4 hover:text-pink-300">
+                    <Link to="/beers" className="text-xl text-white ml-4 hover:text-pink-300">
                         Beers
                     </Link>
                 </div>
