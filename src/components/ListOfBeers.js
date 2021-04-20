99120072c0f841e47a3f0552a9e0979bced4bb4b
gusjak <70512255+gusjak@users.noreply.github.com>
2021-04-20 09:35:32 +0200

    Update ListOfBeers.js

--- src/components/ListOfBeers.js
+++ src/components/ListOfBeers.js
@@ -4,7 +4,10 @@ import { Link } from '@reach/router'
 const ListOfBeers = (props) => {
     return (
         <Link className="transform transition duration-300 hover:scale-105" to={`/beers/${props.beer.id}`}>
-            <div className="min-h-full container mx-auto max-w-sm rounded-lg overflow-hidden shadow-lg py-6 bg-gradient-to-l from-indigo-900 to-purple-800">
+            <div
+                className="transition duration-1000 ease-in-out transform min-h-full container mx-auto max-w-sm rounded-lg overflow-hidden 
+                shadow-lg py-6 bg-gradient-to-l from-indigo-900 to-purple-800 hover:from-indigo-700 hover:to-purple-600"
+            >
                 <img className="object-contain h-56 w-full" src={props.beer.image_url} alt="brewdog beer" />
                 <div className="flex flex-col justify-center items-center pt-10 text-gray-600 text-center">
                     <h2 className="font-bold text-pink-300 tracking-wider text-sm">{props.beer.abv}%</h2>
